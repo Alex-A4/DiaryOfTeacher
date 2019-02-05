3928898581a0c7ffbba93321025584f36e5a9d89
Alex <alex.A4.25@yandex.ru>
2019-02-05 22:49:46 +0300

    Fixed image displaying

--- lib/src/ui/main/welcome_screen.dart
+++ lib/src/ui/main/welcome_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-
 class WelcomeScreen extends StatefulWidget {
   final String uid;
   final String photoUrl;
@@ -18,34 +17,40 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Добро пожаловать'),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: handleSignOut,
-          )
-        ],
-      ),
-
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            CachedNetworkImage(
-              imageUrl: widget.photoUrl,
-              placeholder: Container(child: CircularProgressIndicator(backgroundColor: Color(0xFFDFFAF0),)),
-              width: 300.0,
-              fit: BoxFit.fitWidth,
-            ),
+        appBar: AppBar(
+          title: Text('Добро пожаловать'),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: handleSignOut,
+            )
           ],
         ),
-      ),
-    );
-  }
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CachedNetworkImage(
+                imageUrl: widget.photoUrl,
+                placeholder: Container(
+                    child: CircularProgressIndicator(
+                  backgroundColor: Color(0xFFDFFAF0),
+                )),
+                width: 150.0,
+                fit: BoxFit.fitWidth,
+              ),
 
+              //Expand column to full size of screen
+              Flexible(
+                fit: FlexFit.tight,
+                child: Container(),
+              ),
+            ],
+          ),
+        ));
+  }
 
   Future<Null> handleSignOut() async {
     GoogleSignIn googleSignIn = GoogleSignIn();
@@ -56,6 +61,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
     Navigator.of(context).pushAndRemoveUntil(
         MaterialPageRoute(builder: (context) => SignIn()),
-            (Route<dynamic> route) => false);
+        (Route<dynamic> route) => false);
   }
 }
