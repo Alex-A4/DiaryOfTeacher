a47f6ddd9436beadabb2a675128e5e771972a869
Alex <alex.A4.25@yandex.ru>
2019-02-05 21:28:42 +0300

    Added logic to verify password

--- lib/src/ui/login.dart
+++ lib/src/ui/login.dart
@@ -1,33 +1,80 @@
+import 'package:diary_of_teacher/src/ui/sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LogIn extends StatefulWidget {
   _LogIn createState() => _LogIn();
 }
 
 class _LogIn extends State<LogIn> {
+  final _formKey = GlobalKey<FormState>();
+  var _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Auth'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () async {
-              SharedPreferences pref = await SharedPreferences.getInstance();
-              pref.clear();
-            },
-          )
+        centerTitle: true,
+        title: Text('Вход'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(32.0),
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/login_picture.png',
+                fit: BoxFit.fitHeight,
+                height: 200.0,
+              ),
+              Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                      hintText: 'Введите пароль',
+                    ),
+                    controller: _passwordController,
+                    obscureText: true,
+                    keyboardType: TextInputType.number,
+                    validator: (pass) {
+                      if (pass.isEmpty) return 'Введите пароль';
+                      if (pass.length != 6)
+                        return 'Пароль должен состоять из 6 символов';
+                    },
+                    textAlign: TextAlign.center,
+                    maxLength: 6,
+                  )),
+            ],
+          ),
         ],
       ),
-
-      body: Container(
-        child: Center(
-          child: Text('Good luck'),
-        ),
-      ),
     );
+  }
+
+  Future<Null> handleSignOut() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.disconnect();
+    await googleSignIn.signOut();
+
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => SignIn()),
+        (Route<dynamic> route) => false);
   }
 }
