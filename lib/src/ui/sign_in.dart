e2402e6bc806d40915c46abbdef257254a7be271
Alex <alex.A4.25@yandex.ru>
2019-02-05 21:01:26 +0300

    Fixed keyboard bug

--- lib/src/ui/sign_in.dart
+++ lib/src/ui/sign_in.dart
@@ -6,7 +6,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class SignIn extends StatefulWidget {
   @override
   _SignInState createState() => _SignInState();
@@ -14,6 +13,8 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   SharedPreferences prefs;
+
+  //For authentication
   final GoogleSignIn _gsi = GoogleSignIn();
   final FirebaseAuth _fbAuth = FirebaseAuth.instance;
 
@@ -21,23 +22,50 @@ class _SignInState extends State<SignIn> {
   bool isLoading = false;
   bool isLoggedIn = false;
 
+  //For building password
+  final _formKey = GlobalKey<FormState>();
+  final _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   Future<Null> handleSignIn() async {
     prefs = await SharedPreferences.getInstance();
 
-    this.setState((){
+    this.setState(() {
       isLoading = true;
     });
 
     GoogleSignInAccount googleUser = await _gsi.signIn();
+    //If user not select an account
+    if (googleUser == null) {
+      this.setState(() {
+        isLoading = false;
+      });
+      return;
+    }
+
     GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    FirebaseUser user = await _fbAuth.signInWithGoogle(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+    FirebaseUser user = await _fbAuth.signInWithGoogle(
+        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
 
     if (user != null) {
       // Check is already signed up
-      final QuerySnapshot result =
-      await Firestore.instance
-          .collection('users').where('id', isEqualTo: user.uid).getDocuments();
+      final QuerySnapshot result = await Firestore.instance
+          .collection('users')
+          .where('id', isEqualTo: user.uid)
+          .getDocuments();
       final List<DocumentSnapshot> documents = result.documents;
+
+      currentUser = user;
 
       if (documents.length == 0) {
         // Update data to server if new user
@@ -47,13 +75,14 @@ class _SignInState extends State<SignIn> {
             .setData({'photoUrl': user.photoUrl, 'id': user.uid});
 
         // Write data to local
-        currentUser = user;
+        print('UserId: ${currentUser.uid}\nPhotoUrl: ${currentUser.photoUrl}');
         await prefs.setString('id', currentUser.uid);
         await prefs.setString('photoUrl', currentUser.photoUrl);
       } else {
         // Write data to local
         await prefs.setString('id', documents[0]['id']);
         await prefs.setString('photoUrl', documents[0]['photoUrl']);
+        await prefs.setInt('passwordHash', documents[0]['passwordHash']);
       }
       Fluttertoast.showToast(msg: "Sign in success");
       this.setState(() {
@@ -72,77 +101,93 @@ class _SignInState extends State<SignIn> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return isLoggedIn ?
-         getPasswordBuilding()
-        : getSignIn();
+    return isLoggedIn ? getPasswordBuilding() : getSignIn();
   }
 
-
-  final _formKey = GlobalKey<FormState>();
-  String _password;
-  var _passwordController = TextEditingController();
-
+  //Screen to set password for user
   Widget getPasswordBuilding() {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(''),
+        title: Text('Придумайте пароль'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/login_picture.jpg',
-              fit: BoxFit.fitWidth,
-            ),
-
-            Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text('Введите пароль'),
-                  TextFormField(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/login_picture.png',
+                  height: 200.0,
+                  fit: BoxFit.fitHeight,
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Form(
+                  key: _formKey,
+                  child: TextFormField(
                     decoration: InputDecoration(
                       contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                      hintText: 'Введите пароль',
                     ),
                     controller: _passwordController,
                     obscureText: true,
-                    keyboardType: TextInputType.numberWithOptions(),
+                    keyboardType: TextInputType.number,
                     validator: (pass) {
                       if (pass.isEmpty) return 'Введите пароль';
+                      if (pass.length != 6)
+                        return 'Пароль должен состоять из 6 символов';
                     },
                     textAlign: TextAlign.center,
                     maxLength: 6,
                   ),
+                ),
+                RaisedButton(
+                  child: Text(
+                    'Сохранить',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      SharedPreferences prefs =
+                          await SharedPreferences.getInstance();
+                      //Writing hashcode of password to stores
+                      Firestore.instance
+                          .collection('users')
+                          .document(currentUser.uid)
+                          .updateData({
+                        'passwordHash': _passwordController.text.hashCode
+                      });
 
-                  RaisedButton(
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (_)=> LogIn())
-                        );
-                      }
-                    },
-                  )
-                ],
-              ),
+                      prefs.setInt(
+                          'passwordHash', _passwordController.text.hashCode);
+
+                      print(
+                          'PasswordHash: ${_passwordController.text.hashCode}');
+
+                      Fluttertoast.showToast(
+                          msg: 'Пароль сохранён',
+                          gravity: ToastGravity.BOTTOM,
+                          textColor: Colors.black);
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (_) => LogIn()));
+                    }
+                  },
+                )
+              ],
             ),
-
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
-
+  //Screen to sign in with Google
   Widget getSignIn() {
     return Scaffold(
         appBar: AppBar(
@@ -150,6 +195,7 @@ class _SignInState extends State<SignIn> {
           title: Text('Авторизация'),
         ),
         body: Stack(
+          fit: StackFit.expand,
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(32),
@@ -157,27 +203,31 @@ class _SignInState extends State<SignIn> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Image.asset(
-                    'assets/images/login_picture.jpg',
-                    fit: BoxFit.fitWidth,
+                    'assets/images/login_picture.png',
+                    height: 200.0,
+                    fit: BoxFit.fitHeight,
                   ),
-
+                  SizedBox(
+                    height: 50.0,
+                  ),
                   RaisedButton(
                     onPressed: handleSignIn,
                     child: Text('войти с помощью гугл'.toUpperCase()),
                     elevation: 5.0,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6.0)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6.0)),
                   ),
                 ],
               ),
             ),
-
-            isLoading ?
-            Center(child: CircularProgressIndicator(backgroundColor: Color(0xFFF0FFF0),),)
+            isLoading
+                ? Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Color(0xFFF0FFF0),
+                    ),
+                  )
                 : Container(),
           ],
-        )
-    );
+        ));
   }
-
-
 }
