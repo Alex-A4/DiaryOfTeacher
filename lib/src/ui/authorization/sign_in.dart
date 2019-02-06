05881802581f45d68067fa6c7250b1687c0af8a8
Alex <alex.A4.25@yandex.ru>
2019-02-07 00:34:28 +0300

    Fixed future builder

--- lib/src/ui/authorization/sign_in.dart
+++ lib/src/ui/authorization/sign_in.dart
@@ -1,8 +1,8 @@
 import 'package:diary_of_teacher/src/ui/authorization/password_builder.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import '../../network/authorization.dart';
-
 
 class SignIn extends StatefulWidget {
   @override
@@ -11,15 +11,31 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   FirebaseUser currentUser;
-  bool isLoading = false;
-  bool isLoggedIn = false;
 
   Future<FirebaseUser> userFuture;
 
+  @override
+  void initState() {
+    super.initState();
+    userFuture = null;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return isLoggedIn ? PasswordBuilder(currentUser.uid) : getSignIn();
+    return FutureBuilder<FirebaseUser>(
+      future: userFuture,
+      builder: (_, snapshot) {
+        if (snapshot.hasData) {
+          currentUser = snapshot.data;
+          Fluttertoast.showToast(msg: "Вход выполнен успешно");
+          return PasswordBuilder(currentUser.uid);
+        } else if (snapshot.hasError) {
+          Fluttertoast.showToast(msg: snapshot.error.toString());
+        }
+
+        return getSignIn();
+      },
+    );
   }
 
   //Screen to sign in with Google
@@ -29,65 +45,38 @@ class _SignInState extends State<SignIn> {
           centerTitle: true,
           title: Text('Авторизация'),
         ),
-        body: FutureBuilder(
-          future: userFuture,
-          builder: (_, snapshot) {
-            if (snapshot.hasData) {
-              currentUser = snapshot.data;
-              setState(() {
-                isLoading = false;
-                isLoggedIn = true;
-              });
-            }
-            if (snapshot.hasError) {
-              setState(() {
-                isLoading = false;
-              });
-            }
-
-            return Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(32),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset(
-                        'assets/images/login_picture.png',
-                        height: 200.0,
-                        fit: BoxFit.fitHeight,
-                      ),
-                      SizedBox(
-                        height: 50.0,
-                      ),
-                      RaisedButton(
-                        onPressed: (){
-                          userFuture = handleSignIn();
-                          setState(() {
-                            isLoading = true;
-                          });
-                        },
-                        child: Text('войти с помощью гугл'.toUpperCase()),
-                        elevation: 5.0,
-                        color: Color(0xFFFFE4E1),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6.0)),
-                      ),
-                    ],
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/login_picture.png',
+                    height: 200.0,
+                    fit: BoxFit.fitHeight,
                   ),
-                ),
-                isLoading
-                    ? Center(
-                  child: CircularProgressIndicator(
-                    backgroundColor: Color(0xFFF0FFF0),
+                  SizedBox(
+                    height: 100.0,
                   ),
-                )
-                    : Container(),
-              ],
-            );
-          },
-        ),
-    );
+                  RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                      });
+                      userFuture = handleSignIn();
+                    },
+                    child: Text('войти с помощью гугл'.toUpperCase()),
+                    elevation: 5.0,
+                    color: Color(0xFFFFE4E1),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6.0)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
