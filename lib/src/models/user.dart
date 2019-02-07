3ac661b0834815d7ee259199eef41f0a81950275
Alex <alex.A4.25@yandex.ru>
2019-02-07 15:56:00 +0300

    Changed logic of building instance

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,29 +1,26 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 //Singleton instance of user data
 class User {
   static User _user;
-  static User getUser() {
-    if (_user == null)
-      _user = User.fromStore();
-
-    return _user;
-  }
-
+  static User get user => _user;
 
   String _uid;
   String _photoUrl;
   String _userName;
 
+
+  //Building user data from cache
+  static Future<Null> buildUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _user = User.fromStore(prefs);
+  }
+
   //Constructor to restore user from cache
-  User.fromStore() {
-    SharedPreferences.getInstance()
-        .then((prefs){
-          _uid = prefs.getString('id');
-          _photoUrl = prefs.getString('photoUrl');
-          _userName = prefs.getString('userName');
-    });
+  User.fromStore(SharedPreferences prefs) {
+    _uid = prefs.getString('id');
+    _photoUrl = prefs.getString('photoUrl');
+    _userName = prefs.getString('userName');
   }
 
   String get userName => _userName;
