dc1cbeac807eba39304b9850af3fa2dad9cb194b
Alex <alex.A4.25@yandex.ru>
2019-02-08 19:58:11 +0300

    Renamed to MenuDrawer

--- lib/src/ui/menu/drawer.dart
+++ lib/src/ui/menu/drawer.dart
@@ -5,7 +5,7 @@ import 'package:diary_of_teacher/src/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyDrawer extends StatelessWidget {
+class MenuDrawer extends StatelessWidget {
   MenuBloc menuBloc;
 
   @override
