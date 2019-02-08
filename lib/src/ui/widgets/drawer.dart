39d7f903922f07bb553ff5505d79f17c5dca84bf
Alex <alex.A4.25@yandex.ru>
2019-02-08 15:25:29 +0300

    Added tracker of current window

--- lib/src/ui/widgets/drawer.dart
+++ lib/src/ui/widgets/drawer.dart
@@ -17,8 +17,10 @@ class MyDrawer extends StatelessWidget {
               expandedHeight: 150.0,
               flexibleSpace: GestureDetector(
                 onTap: () {
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                  Windows.moveTo(Window.Profile, context, () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => ProfileScreen()));
+                  });
                 },
                 child: Material(
                   elevation: 5.0,
@@ -51,31 +53,48 @@ class MyDrawer extends StatelessWidget {
             SliverList(
               delegate: SliverChildListDelegate(
                 <Widget>[
-                  SizedBox(height: 25.0,),
+                  SizedBox(
+                    height: 25.0,
+                  ),
                   ListTile(
                     leading: Icon(Icons.event),
                     title: Text('Расписание', style: theme.textTheme.display1),
-                    onTap: () {},
+                    onTap: () {
+                      Windows.moveTo(Window.Schedule, context, (){
+                      });
+                    },
                   ),
                   ListTile(
                     leading: Icon(Icons.people),
                     title: Text('Ученики', style: theme.textTheme.display1),
-                    onTap: () {},
+                    onTap: () {
+                      Windows.moveTo(Window.Schedule, context, (){
+                      });
+                    },
                   ),
                   ListTile(
                     leading: Icon(Icons.featured_play_list),
                     title: Text('Уроки', style: theme.textTheme.display1),
-                    onTap: () {},
+                    onTap: () {
+                      Windows.moveTo(Window.Schedule, context, (){
+                      });
+                    },
                   ),
                   ListTile(
                     leading: Icon(Icons.access_time),
                     title: Text('Перерыв', style: theme.textTheme.display1),
-                    onTap: () {},
+                    onTap: () {
+                      Windows.moveTo(Window.Schedule, context, (){
+                      });
+                    },
                   ),
                   ListTile(
                     leading: Icon(Icons.settings),
                     title: Text('Настройки', style: theme.textTheme.display1),
-                    onTap: () {},
+                    onTap: () {
+                      Windows.moveTo(Window.Schedule, context, (){
+                      });
+                    },
                   ),
                 ],
               ),
@@ -84,5 +103,46 @@ class MyDrawer extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+enum Window {
+  Profile,
+  Schedule,
+  Students,
+  Lessons,
+  Timeout,
+  Settings,
+}
+
+class Windows {
+  Window _currentWindow;
+
+  set currentWindow(Window value) => _currentWindow = value;
+
+  Window get currentWindow => _currentWindow;
+
+  //Singleton instance
+  static Windows _window;
+
+  //Close drawer and move to another window
+  static void moveTo(
+      Window nextWindow, BuildContext context, Function func) async {
+    if (_window == null) {
+      _window = Windows();
+    }
+    _window.currentWindow = nextWindow;
+
+    _closeDrawer(context);
+    //Wait while drawer is not close
+    await Future.delayed(Duration(seconds: 1));
+
+    //If we choose another window then open it
+    if (_window.currentWindow != nextWindow) func();
+  }
+
+  //Close the drawer and go to new screen
+  static void _closeDrawer(context) {
+    Navigator.of(context).pop();
   }
 }
