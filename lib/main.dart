b8da300243b8829c0d93b22baeb3938fb0900f50
Alex <alex.A4.25@yandex.ru>
2019-02-08 18:30:04 +0300

    Added initializing of UserRepo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:diary_of_teacher/src/app.dart';
+import 'package:diary_of_teacher/src/repository/UserRepository.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(App(userRepository: UserRepository(),));
 }
