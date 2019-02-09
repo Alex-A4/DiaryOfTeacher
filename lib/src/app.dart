74a05ee210bb8f15b4fb62d09ca38abf15d8fca4
Alex <alex.A4.25@yandex.ru>
2019-02-09 22:08:40 +0300

    Added textStyle for list subtitle

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -92,5 +92,6 @@ final theme = ThemeData(
         TextStyle(color: Colors.black, fontSize: 18.0, letterSpacing: 0.0),
     //For text in ListTile
     display2: TextStyle(color: Colors.black, fontSize: 17.0),
+    display3: TextStyle(color: Colors.black54, fontSize: 14.0, letterSpacing: 0.0),
   ),
 );
