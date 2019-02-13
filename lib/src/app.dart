fb259438bb265741f26269d1b596747bfffcf28d
Alex <alex.A4.25@yandex.ru>
2019-02-13 20:00:27 +0300

    Deleted letter spacing at body1 param

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -84,7 +84,7 @@ final theme = ThemeData(
   hintColor: Colors.black,
   textTheme: TextTheme(
     //For TextField
-    body1: TextStyle(color: Colors.black, fontSize: 23.0, letterSpacing: 5.0),
+    body1: TextStyle(color: Colors.black, fontSize: 23.0, letterSpacing: 0.0),
     //For button's text
     body2: TextStyle(color: Colors.black, fontSize: 20.0, letterSpacing: 1.0),
     //For Text widgets in menu
