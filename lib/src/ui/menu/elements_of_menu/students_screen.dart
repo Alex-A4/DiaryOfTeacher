3db2116324dd249ce3323dc8b1a67f22e2fc1a0f
Alex <alex.A4.25@yandex.ru>
2019-02-14 10:24:22 +0300

    Moved students list to own file

--- lib/src/ui/menu/elements_of_menu/students_screen.dart
+++ lib/src/ui/menu/elements_of_menu/students_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:diary_of_teacher/src/app.dart';
 import 'package:diary_of_teacher/src/controllers/students_controller.dart';
 import 'package:diary_of_teacher/src/models/student.dart';
@@ -11,9 +10,18 @@ class StudentsScreen extends StatefulWidget {
   _StudentsState createState() => _StudentsState();
 }
 
-class _StudentsState extends State<StudentsScreen> {
+class _StudentsState extends State<StudentsScreen> with SingleTickerProviderStateMixin {
+  TabController _tabController;
   StudentsController _controller = StudentsController.getInstance();
-  PageStorageKey _key = PageStorageKey('StudentsListKey');
+
+  static const List<String> _tabs = ['Ученики', 'Группы'];
+
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: _tabs.length, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,121 +41,16 @@ class _StudentsState extends State<StudentsScreen> {
             tooltip: 'Сохранить на диск',
           ),
         ],
+        bottom: TabBar(
+          tabs: _tabs.map((tab) => Tab(text: tab,)).toList(),
+          controller: _tabController,
+        ),
       ),
-      body: ListView.builder(
-        key: _key,
-        itemCount: _controller.listOfStudents.length,
-        itemBuilder: (context, index) {
-          Student student = _controller.listOfStudents[index];
+      body: TabBarView(
+        //Add students list or groups list
+      ),
 
-          return ListTile(
-            onTap: () {
-              _openStudentEditor(context, false, student: student);
-            },
-            leading: CircleAvatar(
-              backgroundImage: CachedNetworkImageProvider(student.photoUrl),
-            ),
-            title: Text(
-              student.fio,
-              overflow: TextOverflow.ellipsis,
-              style: theme.textTheme.display2,
-            ),
-            subtitle: Text(
-              _controller.getGroupNameById(student.groupId),
-              style: theme.textTheme.display3,
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                IconButton(
-                  tooltip: 'В архив',
-                  icon: Icon(Icons.archive),
-                  onPressed: () {
-                    acceptAction('Добавить в архив?',
-                        'Вы действительно хотите добавиь ученика в архив?', () {
-                      _controller.archiveStudent(student).then((_) {
-                        Fluttertoast.showToast(msg: 'Ученик добавлен у архив');
-                        setState(() {});
-                      }).catchError(
-                          (error) => Fluttertoast.showToast(msg: error));
-                    });
-                  },
-                ),
-                IconButton(
-                  tooltip: 'Удалить',
-                  icon: Icon(Icons.delete),
-                  onPressed: () {
-                    acceptAction('Удалить?',
-                        'Вы действительно хотите удалить этого ученика безвозвратно?',
-                        () {
-                      _controller.deleteStudent(student).then((_) {
-                        Fluttertoast.showToast(msg: 'Ученик удален');
-                        setState(() {});
-                      }).catchError(
-                          (error) => Fluttertoast.showToast(msg: error));
-                    });
-                  },
-                ),
-              ],
-            ),
-          );
-        },
-        padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
-      ),
       drawer: MenuDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _openStudentEditor(context, true);
-        },
-        tooltip: 'Добавить ученика',
-        child: Icon(Icons.add),
-      ),
     );
-  }
-
-  //Open editor to add new student
-  //Pass isEditing to allow editing for now
-  _openStudentEditor(context, bool isEdit, {Student student}) async {
-    await Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => StudentEditor(
-              isEditing: true,
-              student: student,
-            )));
-    setState(() {});
-  }
-
-  void acceptAction(String title, String text, Function func) {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            title: Text(title),
-            content: Text(text),
-            actions: <Widget>[
-              FlatButton(
-                child: Text(
-                  'Отменить',
-                  style: theme.textTheme.body2,
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Принять',
-                  style: theme.textTheme.body2,
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  func();
-                },
-              ),
-            ],
-          );
-        });
   }
 }
---
+++ lib/src/ui/menu/students_subelements/students_list.dart
@@ -0,0 +1,139 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:diary_of_teacher/src/app.dart';
+import 'package:diary_of_teacher/src/controllers/students_controller.dart';
+import 'package:diary_of_teacher/src/models/student.dart';
+import 'package:diary_of_teacher/src/ui/menu/students_subelements/StudentEditor.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+
+class StudentsListView extends StatefulWidget {
+  @override
+  _StudentsListViewState createState() => _StudentsListViewState();
+}
+
+class _StudentsListViewState extends State<StudentsListView> {
+  StudentsController _controller = StudentsController.getInstance();
+  PageStorageKey _key = PageStorageKey('StudentsListKey');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        key: _key,
+        itemCount: _controller.listOfStudents.length,
+        itemBuilder: (context, index) {
+          Student student = _controller.listOfStudents[index];
+
+          return ListTile(
+            onTap: () {
+              _openStudentEditor(context, false, student: student);
+            },
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(student.photoUrl),
+            ),
+            title: Text(
+              student.fio,
+              overflow: TextOverflow.ellipsis,
+              style: theme.textTheme.display2,
+            ),
+            subtitle: Text(
+              _controller.getGroupNameById(student.groupId),
+              style: theme.textTheme.display3,
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  tooltip: 'В архив',
+                  icon: Icon(Icons.archive),
+                  onPressed: () {
+                    acceptAction('Добавить в архив?',
+                        'Вы действительно хотите добавиь ученика в архив?', () {
+                          _controller.archiveStudent(student).then((_) {
+                            Fluttertoast.showToast(msg: 'Ученик добавлен у архив');
+                            setState(() {});
+                          }).catchError(
+                                  (error) => Fluttertoast.showToast(msg: error));
+                        });
+                  },
+                ),
+                IconButton(
+                  tooltip: 'Удалить',
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    acceptAction('Удалить?',
+                        'Вы действительно хотите удалить этого ученика безвозвратно?',
+                            () {
+                          _controller.deleteStudent(student).then((_) {
+                            Fluttertoast.showToast(msg: 'Ученик удален');
+                            setState(() {});
+                          }).catchError(
+                                  (error) => Fluttertoast.showToast(msg: error));
+                        });
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+        padding: EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _openStudentEditor(context, true);
+        },
+        tooltip: 'Добавить ученика',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+
+  //Open editor to add new student
+  //Pass isEditing to allow editing for now
+  _openStudentEditor(context, bool isEdit, {Student student}) async {
+    await Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => StudentEditor(
+          isEditing: true,
+          student: student,
+        )));
+    setState(() {});
+  }
+
+  void acceptAction(String title, String text, Function func) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            title: Text(title),
+            content: Text(text),
+            actions: <Widget>[
+              FlatButton(
+                child: Text(
+                  'Отменить',
+                  style: theme.textTheme.body2,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Text(
+                  'Принять',
+                  style: theme.textTheme.body2,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  func();
+                },
+              ),
+            ],
+          );
+        });
+  }
+}
