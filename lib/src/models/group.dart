19bdf7ee0586bcaa8a725b1607585313b2292af0
Alex <alex.A4.25@yandex.ru>
2019-02-15 13:24:06 +0300

    Added method to update group name

--- lib/src/models/group.dart
+++ lib/src/models/group.dart
@@ -1,46 +1,44 @@
 import 'package:uuid/uuid.dart';
 import 'package:diary_of_teacher/src/models/student.dart';
 
-
 //Class describes group of students
 //Uses only runtime for easy access to sorted users
 class Group {
   final String _groupId;
-  final String _name;
+  String _name;
 
   List<Student> _students = [];
 
-  Group(this._name):
-      this._groupId = Uuid().v1();
+  Group(this._name) : this._groupId = Uuid().v1();
 
-  Group.fromJson(Map<String, dynamic> data) :
-      _groupId = data['groupId'],
-      _name = data['groupName'],
-      _students = [];
+  Group.fromJson(Map<String, dynamic> data)
+      : _groupId = data['groupId'],
+        _name = data['groupName'],
+        _students = [];
 
   Map<String, dynamic> toJson() => {
-    'groupId' : _groupId,
-    'groupName' : _name,
-  };
+        'groupId': _groupId,
+        'groupName': _name,
+      };
+
+  set name(String value) => _name = value;
 
   String get name => _name;
 
   String get groupId => _groupId;
 
-
   @override
-  String toString() => 'UID: $_groupId\nSTUDENTS IN GROUP: ${_students.toString()}\n';
+  String toString() =>
+      'UID: $_groupId\nSTUDENTS IN GROUP: ${_students.toString()}\n';
 
   //Add student to that group
   void addStudentToGroup(Student student) {
     ///Check is student already added
     bool isContains = false;
     _students.forEach((stud) {
-      if (stud.uid == student.uid)
-        isContains = true;
+      if (stud.uid == student.uid) isContains = true;
     });
-    if (!isContains)
-      _students.add(student);
+    if (!isContains) _students.add(student);
 
     student.groupId = _groupId;
   }
@@ -56,5 +54,10 @@ class Group {
   void deleteAllStudents() {
     _students.forEach((stud) => stud.groupId = null);
     _students = null;
+  }
+
+  //Update student name
+  void updateName(String name) {
+    this.name = name;
   }
 }
