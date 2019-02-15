d65f89adb22b5914e8db9fae00c67936f8c0c4d0
Alex <alex.A4.25@yandex.ru>
2019-02-15 13:01:59 +0300

    Added opportunity to delete group from list

--- lib/src/ui/menu/students_subelements/groups_list.dart
+++ lib/src/ui/menu/students_subelements/groups_list.dart
@@ -2,6 +2,7 @@ import 'package:diary_of_teacher/src/app.dart';
 import 'package:diary_of_teacher/src/controllers/students_controller.dart';
 import 'package:diary_of_teacher/src/models/group.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class GroupsListView extends StatefulWidget {
   @override
@@ -27,6 +28,16 @@ class _GroupsListViewState extends State<GroupsListView> {
               print(group.name);
             },
             title: Text(group.name, style: theme.textTheme.body1),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                _controller.deleteGroup(group).then((_) {
+                  Fluttertoast.showToast(msg: 'Группа удалена');
+                  setState(() {});
+                }).catchError((error) => Fluttertoast.showToast(msg: error));
+              },
+              tooltip: 'Удалить группу',
+            ),
           );
         },
       ),
@@ -47,7 +58,8 @@ class _GroupsListViewState extends State<GroupsListView> {
                 borderRadius: BorderRadius.circular(10.0)),
             child: Container(
               height: 200.0,
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -69,8 +81,7 @@ class _GroupsListViewState extends State<GroupsListView> {
                           .addNewGroup(Group(_groupNameController.text));
                       _groupNameController.clear();
                       Navigator.of(context).pop();
-                      setState(() {
-                      });
+                      setState(() {});
                     },
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(7.0)),
