b2eeec7a540394647366e1848324a70944752ca3
Alex <alex.A4.25@yandex.ru>
2019-02-26 15:46:53 +0300

    Changed time storeing

--- lib/src/models/lesson.dart
+++ lib/src/models/lesson.dart
@@ -1,34 +1,39 @@
 import 'package:diary_of_teacher/src/controllers/students_controller.dart';
 import 'package:diary_of_teacher/src/models/group.dart';
 import 'package:decimal/decimal.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 //Class that describes one lesson
 //That could be convert to/from Json
 class Lesson {
   int weekDay;
-  DateTime lessonTime;
+  DateTime lessonDate;
   Group groupToStudy;
   String theme;
   String homeWork;
   Decimal earnedMoney;
+  TimeOfDay lessonTime;
 
   //All fields are required
   Lesson(
-      {@required this.lessonTime,
+      {@required this.lessonDate,
       @required this.groupToStudy,
       @required this.theme,
       @required this.homeWork,
+      @required this.lessonTime,
       @required this.earnedMoney})
-      : weekDay = lessonTime.weekday;
+      : weekDay = lessonDate.weekday;
 
   //Convert lesson object to Json data
   Map<String, dynamic> toJson() {
     return {
       'weekDay': weekDay,
-      'lessonTime': lessonTime.toString(),
+      'lessonDate': lessonDate.toString(),
       'groupToStudy': groupToStudy?.groupId,
       'theme': theme,
+      'lessonHour': lessonTime.hour,
+      'lessonMin': lessonTime.minute,
       'homeWork': homeWork,
       'earnedMoney': earnedMoney.toString()
     };
@@ -36,22 +41,25 @@ class Lesson {
 
   //Create lesson object from Json data
   Lesson.fromJson(Map<String, dynamic> data)
-      : lessonTime = DateTime.parse(data['lessonTime']),
+      : lessonDate = DateTime.parse(data['lessonDate']),
         weekDay = data['weekDay'],
-        groupToStudy = StudentsController.getInstance().getGroupById(data['groupToStudy']),
+        groupToStudy =
+            StudentsController.getInstance().getGroupById(data['groupToStudy']),
         theme = data['theme'],
         homeWork = data['homeWork'],
+        lessonTime =
+            TimeOfDay(hour: data['lessonHour'], minute: data['lessonMin']),
         earnedMoney = Decimal.parse(data['earnedMoney']);
 
   @override
   String toString() =>
-      'Lesson: ${groupToStudy?.groupId}, ${lessonTime.toString()}, hw ${homeWork}, theme ${theme}, money ${earnedMoney}';
+      'Lesson: ${groupToStudy?.groupId}, ${lessonDate.toString()}, hw ${homeWork}, theme ${theme}, money ${earnedMoney}';
 
   //Update data
-  void updateData(Decimal earnedMoney, Group group, String hw,
-      DateTime lessonTime, String studyTheme) {
+  void updateData(Decimal earnedMoney, Group group, String hw, TimeOfDay time,
+      String studyTheme) {
     this.earnedMoney = earnedMoney;
-    this.lessonTime = lessonTime;
+    this.lessonTime = time;
     this.groupToStudy = group;
     this.homeWork = hw;
     this.theme = studyTheme;
