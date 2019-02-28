d5a977977122eb0e34abeaa50a5f1d07decf82f6
Alex <alex.A4.25@yandex.ru>
2019-02-28 14:43:17 +0300

    Added Route to open big photo

--- lib/src/ui/menu/lessons_subelements/photo_card.dart
+++ lib/src/ui/menu/lessons_subelements/photo_card.dart
@@ -1,10 +1,9 @@
-import 'package:diary_of_teacher/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
 class PhotoCard extends StatelessWidget {
   PhotoCard(
-      {Key key, @required this.photoUrl, this.width = 100, this.height = 100})
+      {Key key, @required this.photoUrl, this.width = 150, this.height = 100})
       : super(key: key);
 
   double width;
@@ -14,26 +13,27 @@ class PhotoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      elevation: 5.0,
-      child: GestureDetector(
-        onTap: () {
-          Navigator.of(context).push(_PhotoDisplayRoute(
-              barrierLabel:
-                  MaterialLocalizations.of(context).modalBarrierDismissLabel,
-              photoUrl: photoUrl, width: width, height: height));
-        },
-        child: CachedNetworkImage(
-          imageUrl: photoUrl,
-          width: width,
-          height: height,
-          placeholder: Center(
-            child: CircularProgressIndicator(),
+
+    return Container(
+        width: width,
+        height: height,
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(_PhotoDisplayRoute(
+                barrierLabel:
+                    MaterialLocalizations.of(context).modalBarrierDismissLabel,
+                photoUrl: photoUrl));
+          },
+          child: Material(
+            color: Colors.grey[300],
+            elevation: 5.0,
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            child: CachedNetworkImage(
+              imageUrl: photoUrl,
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
@@ -43,7 +43,7 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
   final double height;
 
   @override
-  Color get barrierColor => theme.accentColor;
+  Color get barrierColor => Colors.white24;
 
   @override
   bool get barrierDismissible => true;
@@ -59,7 +59,16 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
       removeRight: true,
       context: context,
       child: Center(
-        child: CachedNetworkImage(imageUrl: photoUrl, width: 400, height: 400,),
+        child: GestureDetector(
+          child: CachedNetworkImage(
+            imageUrl: photoUrl,
+            width: 300,
+            height: 300,
+          ),
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
     );
 
@@ -69,5 +78,6 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
   @override
   Duration get transitionDuration => const Duration(milliseconds: 200);
 
-  _PhotoDisplayRoute({this.barrierLabel, this.photoUrl, this.width, this.height});
+  _PhotoDisplayRoute(
+      {this.barrierLabel, this.photoUrl, this.width, this.height});
 }
