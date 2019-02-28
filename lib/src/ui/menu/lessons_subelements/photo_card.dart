5df7950fcb2ee011493ef33d5f5ea7092f06266f
Alex <alex.A4.25@yandex.ru>
2019-02-28 19:09:25 +0300

    Changed UI. Added blur

--- lib/src/ui/menu/lessons_subelements/photo_card.dart
+++ lib/src/ui/menu/lessons_subelements/photo_card.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -13,27 +14,24 @@ class PhotoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
-        width: width,
-        height: height,
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).push(_PhotoDisplayRoute(
-                barrierLabel:
-                    MaterialLocalizations.of(context).modalBarrierDismissLabel,
-                photoUrl: photoUrl));
-          },
-          child: Material(
-            color: Colors.grey[300],
-            elevation: 5.0,
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            child: CachedNetworkImage(
-              imageUrl: photoUrl,
-            ),
-          ),
-        ));
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(_PhotoDisplayRoute(
+              photoUrl: photoUrl));
+        },
+        child: Container(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                  image: CachedNetworkImageProvider(photoUrl),
+                  fit: BoxFit.cover)),
+        ),
+      ),
+    );
   }
 }
 
@@ -45,11 +43,13 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
   @override
   Color get barrierColor => Colors.white24;
 
+
   @override
   bool get barrierDismissible => true;
 
   @override
-  final String barrierLabel;
+  String get barrierLabel => 'Photo';
+
 
   @override
   Widget buildPage(BuildContext context, Animation<double> animation,
@@ -58,26 +58,40 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
       removeLeft: true,
       removeRight: true,
       context: context,
-      child: Center(
         child: GestureDetector(
-          child: CachedNetworkImage(
-            imageUrl: photoUrl,
-            width: 300,
-            height: 300,
+          child: Container(
+            constraints: const BoxConstraints.expand(),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaY: 3.0, sigmaX: 3.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.black.withOpacity(0.3)),
+                child: Center(
+                  child: Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: CachedNetworkImageProvider(photoUrl),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
           onTap: () {
             Navigator.of(context).pop();
           },
         ),
-      ),
     );
 
     return photoDialog;
   }
 
   @override
-  Duration get transitionDuration => const Duration(milliseconds: 200);
+  Duration get transitionDuration => const Duration(milliseconds: 100);
 
   _PhotoDisplayRoute(
-      {this.barrierLabel, this.photoUrl, this.width, this.height});
+      {this.photoUrl, this.width, this.height});
 }
