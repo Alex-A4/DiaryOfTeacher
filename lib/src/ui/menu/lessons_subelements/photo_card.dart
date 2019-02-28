9e4b3c94e35d3fa5e3d2f0c74c08afba602f5541
Alex <alex.A4.25@yandex.ru>
2019-02-28 13:56:25 +0300

    Added tap action on photo

--- lib/src/ui/menu/lessons_subelements/photo_card.dart
+++ lib/src/ui/menu/lessons_subelements/photo_card.dart
@@ -1,3 +1,4 @@
+import 'package:diary_of_teacher/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -17,7 +18,12 @@ class PhotoCard extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       elevation: 5.0,
       child: GestureDetector(
-//        onTap: ,
+        onTap: () {
+          Navigator.of(context).push(_PhotoDisplayRoute(
+              barrierLabel:
+                  MaterialLocalizations.of(context).modalBarrierDismissLabel,
+              photoUrl: photoUrl, width: width, height: height));
+        },
         child: CachedNetworkImage(
           imageUrl: photoUrl,
           width: width,
@@ -29,4 +35,39 @@ class PhotoCard extends StatelessWidget {
       ),
     );
   }
+}
+
+class _PhotoDisplayRoute<T> extends PopupRoute<T> {
+  final String photoUrl;
+  final double width;
+  final double height;
+
+  @override
+  Color get barrierColor => theme.accentColor;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  final String barrierLabel;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    Widget photoDialog = MediaQuery.removePadding(
+      removeLeft: true,
+      removeRight: true,
+      context: context,
+      child: Center(
+        child: CachedNetworkImage(imageUrl: photoUrl, width: 400, height: 400,),
+      ),
+    );
+
+    return photoDialog;
+  }
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 200);
+
+  _PhotoDisplayRoute({this.barrierLabel, this.photoUrl, this.width, this.height});
 }
