4f5e0ed6b200d707d88781a74d638b1cfc262942
Alex <alex.A4.25@yandex.ru>
2019-03-01 17:05:07 +0300

    Added delete button

--- lib/src/ui/menu/lessons_subelements/photo_card.dart
+++ lib/src/ui/menu/lessons_subelements/photo_card.dart
@@ -4,32 +4,70 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 class PhotoCard extends StatelessWidget {
   PhotoCard(
-      {Key key, @required this.photoUrl, this.width = 150, this.height = 100})
-      : super(key: key);
+      {Key key,
+      @required this.photoUrl,
+      this.width = 150,
+      this.height = 100,
+      this.deleteFunc})
+      : super(key: key) {
+    deleteFunc == null ? this.topPadding = 0.0 : this.topPadding = 10.0;
+  }
 
+  VoidCallback deleteFunc;
   double width;
   double height;
+  double topPadding;
 
   String photoUrl;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      child: GestureDetector(
-        onTap: () {
-          Navigator.of(context).push(_PhotoDisplayRoute(
-              photoUrl: photoUrl));
-        },
-        child: Container(
-          height: height,
-          width: width,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              image: DecorationImage(
-                  image: CachedNetworkImageProvider(photoUrl),
-                  fit: BoxFit.cover)),
-        ),
+      width: width,
+      height: height,
+      child: Stack(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(_PhotoDisplayRoute(photoUrl: photoUrl));
+            },
+            child: Container(
+              padding:
+                  EdgeInsets.only(top: topPadding, left: 10.0, right: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[600],
+                    borderRadius: BorderRadius.circular(10.0),
+                    image: DecorationImage(
+                        image: CachedNetworkImageProvider(photoUrl),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+          ),
+          deleteFunc == null
+              ? Container()
+              : Positioned(
+                  right: 0.0,
+                  top: 0.0,
+                  child: Container(
+                      width: 25,
+                      height: 25,
+                      decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0)),
+                          color: Colors.grey[400]),
+                      child: Center(
+                        child: GestureDetector(
+                          onTap: deleteFunc,
+                          child: Icon(
+                            Icons.close,
+                            color: Colors.white,
+                          ),
+                        ),
+                      )),
+                ),
+        ],
       ),
     );
   }
@@ -43,13 +81,11 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
   @override
   Color get barrierColor => Colors.white24;
 
-
   @override
   bool get barrierDismissible => true;
 
   @override
   String get barrierLabel => 'Photo';
-
 
   @override
   Widget buildPage(BuildContext context, Animation<double> animation,
@@ -58,32 +94,32 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
       removeLeft: true,
       removeRight: true,
       context: context,
-        child: GestureDetector(
-          child: Container(
-            constraints: const BoxConstraints.expand(),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaY: 3.0, sigmaX: 3.0),
-              child: Container(
-                decoration: BoxDecoration(color: Colors.black.withOpacity(0.3)),
-                child: Center(
-                  child: Container(
-                    width: 300,
-                    height: 300,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: CachedNetworkImageProvider(photoUrl),
-                          fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+      child: GestureDetector(
+        child: Container(
+          constraints: const BoxConstraints.expand(),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaY: 3.0, sigmaX: 3.0),
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.3)),
+              child: Center(
+                child: Container(
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: CachedNetworkImageProvider(photoUrl),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
                 ),
               ),
             ),
           ),
-          onTap: () {
-            Navigator.of(context).pop();
-          },
         ),
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+      ),
     );
 
     return photoDialog;
@@ -92,6 +128,5 @@ class _PhotoDisplayRoute<T> extends PopupRoute<T> {
   @override
   Duration get transitionDuration => const Duration(milliseconds: 100);
 
-  _PhotoDisplayRoute(
-      {this.photoUrl, this.width, this.height});
+  _PhotoDisplayRoute({this.photoUrl, this.width, this.height});
 }
