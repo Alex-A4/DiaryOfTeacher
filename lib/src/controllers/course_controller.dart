69ae7c6efb57145ac6811f9e732d53b60d235873
Alex <alex.A4.25@yandex.ru>
2019-03-07 20:44:12 +0300

    Added methods to upload/restore data from firestore

--- lib/src/controllers/course_controller.dart
+++ lib/src/controllers/course_controller.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diary_of_teacher/src/models/course.dart';
+import 'package:diary_of_teacher/src/models/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 import '../mixins/network_mixin.dart';
@@ -33,8 +35,7 @@ class CourseController extends ImageUploader {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String line = prefs.getString('courses');
     if (line != null) {
-      List<dynamic> dynCourses = _codec.decode(line);
-      _courses = dynCourses.map((data) => Course.fromJson(data)).toList();
+      _courses = convertJsonToCourses(_codec.decode(line));
     } else
       _courses = [];
   }
@@ -42,7 +43,7 @@ class CourseController extends ImageUploader {
   //Saving all courses to cache
   Future saveToCache() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('courses', _codec.encode(_courses));
+    prefs.setString('courses', _codec.encode(convertListToJson()));
   }
 
   //Upload image to cloud firebase
@@ -52,4 +53,41 @@ class CourseController extends ImageUploader {
 
     return url;
   }
+
+  //Uploading data about courses to cloud firestore
+  Future uploadDataToFirestore() async {
+    if (!await isConnected()) throw 'Отсутствует интернет соединение';
+
+    await Firestore.instance
+        .collection('users')
+        .document(User.user.uid)
+        .collection('courses')
+        .document('courses')
+        .updateData({'courses': convertListToJson()});
+  }
+
+  //Restore data about courses from the cloud firestore
+  Future restoreDataFromFirestore() async {
+    if (!await isConnected()) throw 'Отсутствует интернет соединение';
+
+    DocumentSnapshot snapshot = await Firestore.instance
+        .collection('users')
+        .document(User.user.uid)
+        .collection('courses')
+        .document('courses')
+        .get();
+
+    if (snapshot == null) throw 'Нет данных в облаке';
+    _courses = convertJsonToCourses(snapshot.data['courses']);
+
+    await saveToCache();
+  }
+
+  //Convert list of courses to dynamic list
+  List<Map<String, dynamic>> convertListToJson() =>
+      _courses.map((data) => data.toJson()).toList();
+
+  //Convert dynamic list to list of courses
+  List<Course> convertJsonToCourses(List<dynamic> dynCourses) =>
+      dynCourses.map((data) => Course.fromJson(data)).toList();
 }
