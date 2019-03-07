1e7216e6f5c56e633acae19e5d25b40c2c92f6a1
Alex <alex.A4.25@yandex.ru>
2019-03-07 23:17:33 +0300

    Init course lesson editor

---
+++ lib/src/ui/menu/courses_subelements/course_lesson_editor.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CourseLessonEditor extends StatefulWidget {
+  CourseLessonEditor({Key key}) : super(key: key);
+  @override
+  _CourseLessonEditorState createState() => _CourseLessonEditorState();
+}
+
+class _CourseLessonEditorState extends State<CourseLessonEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
