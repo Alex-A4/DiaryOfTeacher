41b7ef99a86697545e9be4b0dd626f827b4337d5
Alex <alex.A4.25@yandex.ru>
2019-03-07 23:55:51 +0300

    Added init params and dropdown menu

--- lib/src/ui/menu/courses_subelements/course_lesson_editor.dart
+++ lib/src/ui/menu/courses_subelements/course_lesson_editor.dart
@@ -1,14 +1,49 @@
+import 'package:diary_of_teacher/src/models/course.dart';
+import 'package:diary_of_teacher/src/ui/menu/courses_subelements/drowdown_menu.dart';
 import 'package:flutter/material.dart';
 
 class CourseLessonEditor extends StatefulWidget {
-  CourseLessonEditor({Key key}) : super(key: key);
+  CourseLessonEditor({Key key, @required this.course, @required this.lesson})
+      : super(key: key);
+
+  //Course needs to add lesson to course
+  Course course;
+
+  //Lesson needs to modify info
+  CourseLesson lesson;
+
   @override
   _CourseLessonEditorState createState() => _CourseLessonEditorState();
 }
 
 class _CourseLessonEditorState extends State<CourseLessonEditor> {
+  String title;
+  List<String> images;
+  List<String> texts;
+
+  @override
+  void initState() {
+    super.initState();
+    title = widget.lesson?.title ?? 'Название занятия ';
+    images = widget.lesson?.imagesList ?? [];
+    texts = widget.lesson?.textsList ?? [];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Align(
+            alignment: AlignmentDirectional.bottomEnd,
+            child: DropdownMenu(),
+          ),
+        ],
+      ),
+    );
   }
 }
